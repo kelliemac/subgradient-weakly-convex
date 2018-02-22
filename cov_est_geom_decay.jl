9924623b47d90719df5a126cb13ceb25bbc11754
Kellie MacPhee <kmacphee@uw.edu>
2018-02-22 11:37:14 -0800

    Improving the constant and geometric step plots

--- cov_est_geom_decay.jl
+++ cov_est_geom_decay.jl
@@ -9,10 +9,11 @@ include("solve_cov_est.jl");
 #   Parameters
 #--------------------------------------------------------------------
 
-iterMax  = 1000;
+iterMax  = 500;
 m  = 3000;      # number of measurements (need m>2d)
 d  = 1000;       # ambient dimension
 r = 1;           # rank
+σ  = 0.0;      # noise level in observations
 
 #--------------------------------------------------------------------
 #   Generate Data
@@ -23,18 +24,18 @@ A  = randn(d,m); # columns are measurement vectors, entries are iid N(0,1)
 Xtrue = randn(d,r);
 XTtrue = Xtrue.';
 
-distrib = Bernoulli(0.1);
+distrib = Bernoulli(0.01);
 z = rand(distrib, m);
-distrib2 = Normal(0.0, 10.0);
+distrib2 = Normal(0.0, 1.0);
 ζ = rand(distrib2, m);
 
 b = mapslices( x -> sum( abs2, XTtrue * x ) , A, [1])';  # slow
-# b = (1-z) .* b + z .* abs(ζ);
+b = (1-z) .* b + z .* abs(ζ);
 
 #--------------------------------------------------------------------
 #  Generate initial point
 #--------------------------------------------------------------------
-init_radius = 0.01 * vecnorm(Xtrue);  # should be like gamma * mu / rho. make sure to scale with dimension
+init_radius = 1.0 * vecnorm(Xtrue);  # should be like gamma * mu / rho. make sure to scale with dimension
 pert = randn(d,r);
 X0 = Xtrue + (init_radius  / vecnorm(pert) )* pert ;     # so that || X0- Xtrue ||_F = init_radius.
 
@@ -45,15 +46,14 @@ clf();
 xlabel(L"Iteration $k$");
 ylabel(L"$dist \, (X_k,\mathcal{X}^*) \; / \;  || \bar X ||_F$");
 
-# αVals =[1.0, 1/3, 1/9];
-αVals = [1/3];
+ρ_est = 9.0;
+L_est = 50.0 * normXtrue;
+μ_est = 0.1 * normXtrue;
 
-for α in αVals
-        #--------------------------------------------------------------------
-        #  Solve and add to plot
-        #--------------------------------------------------------------------
-         (Xest, obj_hist, err_hist) = solve_cov_est( A, b, X0, Xtrue; OptVal=0.0, iter_max=iterMax, step="Constant", stepSize=α);      # no noise, Polyak step
-        semilogy(err_hist);
-end
+#--------------------------------------------------------------------
+#  Solve and add to plot
+#--------------------------------------------------------------------
+(Xest, obj_hist, err_hist) = solve_cov_est( A, b, X0, Xtrue; OptVal=0.0, iter_max=iterMax, step="Decay", μ= μ_est , L=L_est , ρ=ρ_est );
+semilogy(err_hist);
 
-savefig("constant_step_error.pdf");
+savefig("geom_decay_error.pdf");
